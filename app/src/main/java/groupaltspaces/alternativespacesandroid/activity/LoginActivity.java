74240942e1eb588919e163fbec27fcbb15265226
freva <valerijf@stud.ntnu.no>
2014-10-16 23:30:16 +0200

    Made lots of changes to login form, upgraded to min API 11

--- app/src/main/java/groupaltspaces/alternativespacesandroid/activity/LoginActivity.java
+++ app/src/main/java/groupaltspaces/alternativespacesandroid/activity/LoginActivity.java
@@ -2,24 +2,16 @@ package groupaltspaces.alternativespacesandroid.activity;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
-import java.io.File;
+import android.widget.EditText;
 
 import groupaltspaces.alternativespacesandroid.R;
-import groupaltspaces.alternativespacesandroid.util.FileUtils;
-
 
 public class LoginActivity extends Activity {
-
+    private EditText username;
+    private EditText password;
     private Button login;
     private Context context;
 
@@ -28,5 +20,24 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         context = this;
+
+        bindViews();
+        addButtonListener();
+    }
+
+    private void bindViews(){
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+    }
+
+    private void addButtonListener(){
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println(username.getText());
+                System.out.println(password.getText());
+            }
+        });
     }
 }
