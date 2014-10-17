741cc2dc543bc8df127e611c5daacc1301e4aa0b
Brage Jahren <brage.jahren@gmail.com>
2014-10-17 11:53:20 +0200

    Create LoginTask to handle login server call

--- app/src/main/java/groupaltspaces/alternativespacesandroid/activity/LoginActivity.java
+++ app/src/main/java/groupaltspaces/alternativespacesandroid/activity/LoginActivity.java
@@ -2,24 +2,27 @@ package groupaltspaces.alternativespacesandroid.activity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import groupaltspaces.alternativespacesandroid.R;
+import groupaltspaces.alternativespacesandroid.tasks.LoginTask;
 
 public class LoginActivity extends Activity {
     private EditText username;
     private EditText password;
     private Button login;
-    private Context context;
+    private LoginActivity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        context = this;
+        activity = this;
 
         bindViews();
         addButtonListener();
@@ -35,9 +38,21 @@ public class LoginActivity extends Activity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                LoginTask loginTask = new LoginTask(username.getText().toString(), password.getText().toString(),activity);
+                loginTask.execute();
                 System.out.println(username.getText());
                 System.out.println(password.getText());
             }
         });
+    }
+
+    public void onLoginSuccess(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onLoginFail(){
+            Toast.makeText(getApplicationContext(), "Unable to log in", Toast.LENGTH_SHORT).show();
     }
 }
