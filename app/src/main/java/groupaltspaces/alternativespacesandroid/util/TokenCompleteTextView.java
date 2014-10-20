a72dd5bce01a7cc73ab23848caf679f6be5f4286
Valerij Fredriksen <valerijf@stud.ntnu.no>
2014-10-21 01:14:26 +0200

    Fixed the bug that caused app to crash when changing focus after erasing a token

--- app/src/main/java/groupaltspaces/alternativespacesandroid/util/TokenCompleteTextView.java
+++ app/src/main/java/groupaltspaces/alternativespacesandroid/util/TokenCompleteTextView.java
@@ -17,13 +17,10 @@ import android.text.TextUtils;
 import android.text.method.QwertyKeyListener;
 import android.text.style.ReplacementSpan;
 import android.util.AttributeSet;
-import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.Filter;
 import android.widget.MultiAutoCompleteTextView;
-import android.widget.TextView;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -37,18 +34,9 @@ import java.util.List;
  */
 
 public abstract class TokenCompleteTextView extends MultiAutoCompleteTextView {
-
-    //When the token is deleted...
-    public enum TokenDeleteStyle {
-        Clear, //...clear the underlying text
-        PartialCompletion, //...return the original text used for completion
-        ToString //...replace the token with toString of the token object
-    }
-
     private Tokenizer tokenizer;
     private Object selectedObject;
     private ArrayList<Object> objects;
-    private TokenDeleteStyle deletionStyle = TokenDeleteStyle.Clear;
     private Layout lastLayout = null;
     private boolean initialized = false;
 
@@ -112,13 +100,6 @@ public abstract class TokenCompleteTextView extends MultiAutoCompleteTextView {
      */
     abstract protected View getViewForObject(Object object);
 
-    protected String currentCompletionText() {
-        Editable editable = getText();
-        int end = getSelectionEnd();
-        int start = tokenizer.findTokenStart(editable, end);
-        return TextUtils.substring(editable, start, end);
-    }
-
     private float maxTextWidth() {
         return getWidth() - getPaddingLeft() - getPaddingRight();
     }
@@ -158,7 +139,6 @@ public abstract class TokenCompleteTextView extends MultiAutoCompleteTextView {
         boolean handled = false;
 
         if (isFocused() && text != null && lastLayout != null && action == MotionEvent.ACTION_UP) {
-
             int offset = getOffsetForPosition(event.getX(), event.getY());
 
             if (offset != -1) {
@@ -207,80 +187,15 @@ public abstract class TokenCompleteTextView extends MultiAutoCompleteTextView {
         lastLayout = getLayout(); //Used for checking text positions
     }
 
-    protected void handleFocus(boolean hasFocus) {
-        if (!hasFocus) {
-            setSingleLine(true);
-
-            Editable text = getText();
-            if (text != null && lastLayout != null) {
-                //Display +x thingy if appropriate
-                int lastPosition = lastLayout.getLineVisibleEnd(0);
-                TokenImageSpan[] tokens = text.getSpans(0, lastPosition, TokenImageSpan.class);
-                int count = objects.size() - tokens.length;
-                if (count > 0) {
-                    lastPosition++;
-                    CountSpan cs = new CountSpan(count, getContext(), getCurrentTextColor(),
-                            (int)getTextSize(), (int)maxTextWidth());
-                    text.insert(lastPosition, cs.text);
-
-                    float newWidth = Layout.getDesiredWidth(text, 0,
-                            lastPosition + cs.text.length(), lastLayout.getPaint());
-                    //If the +x span will be moved off screen, move it one token in
-                    if (newWidth > maxTextWidth()) {
-                        text.delete(lastPosition, lastPosition + cs.text.length());
-
-                        if (tokens.length > 0) {
-                            TokenImageSpan token = tokens[tokens.length - 1];
-                            lastPosition = text.getSpanStart(token);
-                            cs.setCount(count + 1);
-                        }
-
-                        text.insert(lastPosition, cs.text);
-                    }
-
-                    text.setSpan(cs, lastPosition, lastPosition + cs.text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                }
-            }
-
-
-        } else {
-            setSingleLine(false);
-            Editable text = getText();
-            if (text != null) {
-                CountSpan[] counts = text.getSpans(0, text.length(), CountSpan.class);
-                for (CountSpan count: counts) {
-                    text.delete(text.getSpanStart(count), text.getSpanEnd(count));
-                    text.removeSpan(count);
-                }
-
-                setSelection(text.length());
-            }
-        }
-    }
-
     @Override
     public void onFocusChanged(boolean hasFocus, int direction, Rect previous) {
         super.onFocusChanged(hasFocus, direction, previous);
-
-        handleFocus(hasFocus);
     }
 
     @Override
     protected CharSequence convertSelectionToString(Object object) {
         selectedObject = object;
-
-        //if the token gets deleted, this text will get put in the field instead
-        switch (deletionStyle) {
-            case Clear:
-                return "";
-            case PartialCompletion:
-                return currentCompletionText();
-            case ToString:
-                return object.toString();
-            default:
-                return super.convertSelectionToString(object);
-
-        }
+        return "";
     }
 
     private SpannableStringBuilder buildSpannableForText(CharSequence text) {
@@ -322,16 +237,15 @@ public abstract class TokenCompleteTextView extends MultiAutoCompleteTextView {
     }
 
 
-
     public void addObject(final Object object) {
-        objects.add(object);
-
         if (object == null) return;
         if (objects.contains(object)) return;
 
-        TokenImageSpan tokenSpan = buildSpanForObject(object);
+        objects.add(object);
 
+        TokenImageSpan tokenSpan = buildSpanForObject(object);
         Editable editable = getText();
+
         if (editable != null) {
             int offset = editable.length();
             editable.setSpan(tokenSpan, offset, offset, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
@@ -397,27 +311,6 @@ public abstract class TokenCompleteTextView extends MultiAutoCompleteTextView {
         }
     }
 
-    private class CountSpan extends ViewSpan {
-        public String text = "";
-        private int count;
-
-        public CountSpan(int count, Context ctx, int textColor, int textSize, int maxWidth) {
-            super(new TextView(ctx));
-            TextView v = (TextView)view;
-            v.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
-            v.setTextColor(textColor);
-            v.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
-            //Make the view as wide as the parent to push the tokens off screen
-            v.setMinimumWidth(maxWidth);
-            setCount(count);
-        }
-
-        public void setCount(int c) {
-            count = c;
-            text = "+" + count;
-            ((TextView)view).setText(text);
-        }
-    }
 
     protected class TokenImageSpan extends ViewSpan {
         private Object token;
@@ -432,9 +325,6 @@ public abstract class TokenCompleteTextView extends MultiAutoCompleteTextView {
         }
 
         public void onClick() {
-            Editable text = getText();
-            if (text == null) return;
-
             removeSpan(this);
         }
     }
@@ -463,7 +353,6 @@ public abstract class TokenCompleteTextView extends MultiAutoCompleteTextView {
         Parcelable superState = super.onSaveInstanceState();
         SavedState state = new SavedState(superState);
 
-        state.tokenDeleteStyle = deletionStyle;
         state.baseObjects = baseObjects;
 
         return state;
@@ -480,22 +369,9 @@ public abstract class TokenCompleteTextView extends MultiAutoCompleteTextView {
         super.onRestoreInstanceState(ss.getSuperState());
 
         setText(ss.prefix);
-        deletionStyle = ss.tokenDeleteStyle;
 
         for (Object obj: convertSerializableArrayToObjectArray(ss.baseObjects)) {
             addObject(obj);
-        }
-
-        //This needs to happen after all the objects get added (which also get posted)
-        //or the view truncates really oddly
-        if (!isFocused()) {
-            post(new Runnable() {
-                @Override
-                public void run() {
-                    //Resize the view nad display the +x if appropriate
-                    handleFocus(isFocused());
-                }
-            });
         }
     }
 
@@ -505,7 +381,6 @@ public abstract class TokenCompleteTextView extends MultiAutoCompleteTextView {
     private static class SavedState extends BaseSavedState {
         String prefix;
         boolean allowDuplicates;
-        TokenDeleteStyle tokenDeleteStyle;
         ArrayList<Serializable> baseObjects;
 
         @SuppressWarnings("unchecked")
@@ -513,7 +388,6 @@ public abstract class TokenCompleteTextView extends MultiAutoCompleteTextView {
             super(in);
             prefix = in.readString();
             allowDuplicates = in.readInt() != 0;
-            tokenDeleteStyle = TokenDeleteStyle.values()[in.readInt()];
             baseObjects = (ArrayList<Serializable>)in.readSerializable();
         }
 
@@ -526,7 +400,6 @@ public abstract class TokenCompleteTextView extends MultiAutoCompleteTextView {
             super.writeToParcel(out, flags);
             out.writeString(prefix);
             out.writeInt(allowDuplicates ? 1 : 0);
-            out.writeInt(tokenDeleteStyle.ordinal());
             out.writeSerializable(baseObjects);
         }
 
